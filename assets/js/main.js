c289401b688b93a75047fd5cb3234a920ebc7f1c
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-01-19 19:54:12 -0300

    working in main

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,14 @@
  * 
  * Acá va solo el manejo del DOM
  * 
- */
+ */ 
+ const rootBox = document.getElementById('root');
+ const dataContainer = (DATA.characters);
+ const printData = (dataContainer) =>{
+     const savePrint = "";
+ }
+dataContainer.forEach => {
+    if(Element.characterName !== ""){
+        savePrint = rootBox.innerHTML
+    }
+}
